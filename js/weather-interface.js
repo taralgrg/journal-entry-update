0225362992939b402faf0daf385352d489bc4ae4
Mac <taralgurung@hotmail.com>
2017-02-15 15:53:03 +0800

    final version after update

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      console.log(response);
+      //console.log(response.main.humidity);
+       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
     });
   });
 });
